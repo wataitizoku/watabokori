dcc6504c762cea12a3f89ea44deeddbd5fecffbb
wataitizoku <zm19141@josai.ac.jp>
2020-10-20 07:25:47 +0000

    1020​

---
+++ ex06-5.js
@@ -0,0 +1,10 @@
+function sayhello06(){
+　var who = 'world';
+　alert('Hello,' + who + '!');
+}
+function taro(){
+　who = '太郎';
+}
+function hanako(){
+　who = '花子';
+}
--- ex6-5.js
+++
@@ -1,8 +0,0 @@
-var who = 'world';
-function sayhello(){alert('Hello,' + who + '!');
-}
-function taro(){
-who = '太郎';}
-function hanako()
-{who = '花子';
-}
