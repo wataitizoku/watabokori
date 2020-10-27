fa4d7ac0fd3584aca9817d13728091567333a2d7
wataitizoku <zm19141@josai.ac.jp>
2020-10-27 12:35:41 +0000

    1027

--- ex6-5.js
+++ ex6-5.js
@@ -1,3 +1,12 @@
-function sayhello65(){var who65 = 'world';alert('Hello,'+ who65 + '!');}  
-function taro65(){var who65 = '太郎';alert(who65 + 'が来た')}
-function hanako65(){var who65 = '花子';alert(who65 + 'が来た')} 
+var who65 = 'world';
+
+function sayhello65() {
+ alert('Hello,' + who65 + '!');
+}
+
+function taro65() {
+ who65 = '太郎';
+}
+function hanako65() {
+ who65 = '花子';
+}
