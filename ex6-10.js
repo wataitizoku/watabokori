870dc931dc65f273b3e91ed9ff72e52e3ae0ae19
wataitizoku <zm19141@josai.ac.jp>
2021-01-19 10:01:03 +0000

    message

--- ex6-10.js
+++ ex6-10.js
@@ -1,1 +1,3 @@
-function imgchange1(){document.getElementById('logo').src='./image/bulbon.jpg';}
+ function imgchange1(){document.getElementById('logo').src='./image/bulbon.jpg';}
+ function imgchange2(){document.getElementById('logo').src='./image/josai.jpg';}
+ 
