f6856b073739d289f8bc6e6ff8eaa3b44109cf95
wataitizoku <zm19141@josai.ac.jp>
2021-01-26 16:55:40 +0000

    message

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,5 @@
 function imgchange1() {
-    document.getElementById('logo').src= './image/josai.jpg';}
+document.getElementById('logo').src= './image/josai.jpg';}
+
+function imgchange2() {
+document.getElementById('logo').src ='./image/bulbon.jpg';}
---
+++ ex7-3.js
@@ -0,0 +1,5 @@
+function add(){
+var x =document.getElementById('input1').Value;
+var y =document.getElementById('input2').Value;
+alert(x+y);
+}
