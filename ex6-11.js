def22d1d45b80d3e9c574de2e0fddbb991146eff
wataitizoku <zm19141@josai.ac.jp>
2021-02-12 15:42:29 +0000

    0213

--- ex6-11.js
+++ ex6-11.js
@@ -1,5 +1,13 @@
-
-function changeImageAndBack(){imgback612();ThreeSecChange612();}
-function ThreeSecChange612(){setTimeout("imgchange612()",3000);}
-function imgchange612(){document.getElementById('img13').src = './image/josai.jpg';}
-function imgback612(){document.getElementById('img13').src = './image/bulbon.jpg';}
+function hyoji1() {
+//表示する文字    
+var str = "一文字ずつ表示します。";
+//テキストボックスの文字数
+var cnt = document.timer1.moji1.value.length;
+//文字が全部表示されている確認
+if ( cnt < 11 ) {
+//現在より1文字多く切り出して表示    
+document.timer1.moji1.value = str.substr(0,cnt+1);} else{
+//全て表示されたら、空文字に戻す    
+document.timer1.moji1.value = "";}  
+//setTimeout()を含む関数を呼び出す  
+setTimeout("hyoji1()",1000) }
---
+++ ex6-12.js
@@ -0,0 +1,4 @@
+function changeImageAndBack(){imgback612();ThreeSecChange612();}
+function ThreeSecChange612(){setTimeout("imgchange612()",3000);}
+function imgchange612(){document.getElementById('img13').src = './image/josai.jpg';}
+function imgback612(){document.getElementById('img13').src = './image/bulbon.jpg';}
