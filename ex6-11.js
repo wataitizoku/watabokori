6dd556ab86dcbe8aff0834847fc9def15034c1ad
wataitizoku <zm19141@josai.ac.jp>
2021-02-12 15:31:47 +0000

    0213

--- ex6-11.js
+++ ex6-11.js
@@ -1,5 +1,5 @@
 
 function changeImageAndBack(){imgback612();ThreeSecChange612();}
 function ThreeSecChange612(){setTimeout("imgchange612()",3000);}
-function imgchange612(){document.getElementById('img13').src = './image/josai.png';}
-function imgback612(){document.getElementById('img13').src = './image/bulbon.gif';}
+function imgchange612(){document.getElementById('img13').src = './image/josai.jpg';}
+function imgback612(){document.getElementById('img13').src = './image/bulbon.jpg';}
